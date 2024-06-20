f4aacd384e01503594ab6be5f334f9930ec9dfb5
Micha <micha-elmans@outlook.com>
2024-06-20 11:08:19 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");{
+namespace Spotivy;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
